c6c194ac736b6454cddb5e47dff4d71a023fbe6e
Script Raccoon <scriptraccoon@gmail.com>
2023-05-04 21:42:12 +0200

    wording in footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -2,10 +2,12 @@
     Stand: <time>02.05.2023</time>. Quelle:
     <a
         href="https://de.wikipedia.org/wiki/Liste_der_Mitglieder_des_Deutschen_Bundestages_%2820._Wahlperiode%29"
-        target="_blank">Wikipedia</a
-    >. Der Code ist auf
-    <a href="https://github.com/ScriptRaccoon/Abgeordnete/" target="_blank"
-        >GitHub</a
+        target="_blank"
+    >
+        Wikipedia</a
+    >. Code:
+    <a href="https://github.com/ScriptRaccoon/Abgeordnete/" target="_blank">
+        GitHub</a
     >.
 </footer>
 
