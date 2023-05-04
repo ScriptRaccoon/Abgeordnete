817ffdaad20dc1a036c83d59264d54fd92f188cc
Script Raccoon <scriptraccoon@gmail.com>
2023-05-04 21:48:23 +0200

    improve JSDoc

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,11 +1,8 @@
-/**
- * @param arr finite sequence of numbers
- * @returns The sequence of partial sums of a finite sequence.
- * For example, [3,5,2,1] returns [0,3,8,10,11].
+/** Computes the sequence of partial sums.
+ *  For example, `[3,5,2,1]` returns `[0,3,8,10,11]`.
+ *  A sequence of length `n` returns a sequence of length `n+1`.
+ * @param sequence finite sequence of numbers
  */
-export function partial_sums(arr: number[]): number[] {
-    return arr.reduce(
-        (list, num) => [...list, list.at(-1) + num],
-        [0]
-    );
+export function partial_sums(sequence: number[]): number[] {
+    return sequence.reduce((list, num) => [...list, list.at(-1) + num], [0]);
 }
